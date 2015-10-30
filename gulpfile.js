9a2292a526b97890de7e528de3fe318933808c33
Riley Banks <waultah@gmail.com>
2015-10-30 16:39:34 +0000

    Added JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp'),
     babel = require('gulp-babel'),
     uglify = require('gulp-uglify'),
-    sourcemaps = require('gulp-sourcemaps');
+    sourcemaps = require('gulp-sourcemaps'),
+    jshint = require('gulp-jshint');
 
 gulp.task('js', function() {
     return gulp.src(['public/assets/js/*.js'])
                // .pipe(sourcemaps.init())
+               .pipe(jshint({ esnext: true }))
+			   .pipe(jshint.reporter('default', { verbose: true }))
                .pipe(babel())
                .pipe(uglify())
                // .pipe(sourcemaps.write())
