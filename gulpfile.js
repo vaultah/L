8ce09d31a63d76709599bde78183c438a149e745
Riley Banks <waultah@gmail.com>
2015-11-05 20:27:23 +0000

    Major re-structuring

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,29 @@ var gulp = require('gulp'),
     babel = require('gulp-babel'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    sass = require('gulp-sass'),
+    rename = require("gulp-rename");
+
 
 gulp.task('js', function() {
-    return gulp.src(['public/assets/js/*.js'])
+    return gulp.src(['src/js/*.js'])
                // .pipe(sourcemaps.init())
                .pipe(jshint({ esnext: true }))
 			         .pipe(jshint.reporter('default', { verbose: true }))
                .pipe(babel())
                .pipe(uglify())
                // .pipe(sourcemaps.write())
-               .pipe(gulp.dest('dist'));
+               .pipe(gulp.dest('public/assets/js'));
 });
 
-gulp.task('default', ['js']);
+
+gulp.task('css', function () {
+    return gulp.src(['src/js/*.css'])
+               .pipe(sass({ style: 'compressed' }))
+               .pipe(rename({ suffix: '.min' }))
+               .pipe(gulp.dest('public/assets/css'));
+});
+
+
+gulp.task('default', ['js', 'css']);
